8ebe0db47cb3b311041c8268984bb1948a1f6671
hamptoam <ameliabedelia94@gmail.com>
2019-08-07 17:26:28 -0500

    Set up the accountviewmodel and trying to update migration.

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -65,9 +65,18 @@ namespace TrashCollection.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "UserRoles")]
+        public string UserRoles { get; set; }
+
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+        
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
@@ -84,9 +93,18 @@ namespace TrashCollection.Models
     public class ResetPasswordViewModel
     {
         [Required]
+        [Display(Name = "UserRoles")]
+        public string UserRoles { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
