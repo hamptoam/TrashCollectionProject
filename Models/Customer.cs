804d467accb915e2497f7051770286be04913e3b
hamptoam <ameliabedelia94@gmail.com>
2019-08-09 09:39:42 -0500

    Greatly improved customer model and details and added migration, working on pickup preference.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,13 +9,27 @@ namespace TrashCollection.Models
     public class Customer
     {
         [Key]
+      
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email" )]
         public string Email { get; set; }
-        public string Address { get; set; }
-        public DateTime timeandDayPreference { get; set; }
 
+        [Display(Name = "Pick-up Address")]
+        public string pickUpAddress { get; set; }
 
+        [Display(Name = "Billing Address")]
+        public string billingAddress { get; set; } //find out how to do "same as billing address? with checkbox
+        
+        [Display(Name = "Day Preference")]
+        public string dayOfWeek { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string phoneNumber { get; set; }
 
     }
 
