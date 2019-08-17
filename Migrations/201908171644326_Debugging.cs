15050797977a016df3c42090cafb1eb0b931dffc
hamptoam <ameliabedelia94@gmail.com>
2019-08-17 17:47:28 -0500

    Fixed errors in viewstart.

---
+++ Migrations/201908171644326_Debugging.cs
@@ -0,0 +1,16 @@
+namespace TrashCollection.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Debugging : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
